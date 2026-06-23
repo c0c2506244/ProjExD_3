7af53e5407488c8a19eb3fb194d1c97b49364a0f
C0C25062 <c0c2506244@edu.teu.ac.jp>
2026-06-23 14:29:05 +0900

    練習２

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -95,7 +95,7 @@ class Beam:
         """
         self.img = pg.image.load(f"fig/beam.png")
         self.rct = self.img.get_rect()
-        self.rct.centery = bird.rct.center #ビームの中心縦座標 = こうかとんの中心縦座標
+        self.rct.centery = bird.rct.centery #ビームの中心縦座標 = こうかとんの中心縦座標
         self.rct.left = bird.rct.right #ビームの左座標 = こうかとんの右座標
         self.vx, self.vy = +5, 0
 
@@ -157,17 +157,26 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        if bomb is not None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+        
+        if bomb is not None:
+            if beam is not None:
+                if beam.rct.colliderect(bomb.rct):
+                    beam = None
+                    bomb = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        beam.update(screen)   
-        bomb.update(screen)
+        if beam is not None:  #beamが出現したら
+            beam.update(screen)
+        if bomb is not None: 
+            bomb.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
